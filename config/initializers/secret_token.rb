27b184b4d6e698fc7f322fd891f3bf5d52b9b9a1
Trunks Li <trunksli@hotmail.com>
2013-11-09 03:52:34 -0500

    Blank version

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Skimly::Application.config.secret_key_base = 'c456c175aac461cd13860e9077c61c73a4b3f9efb171d132ee826167bbeecea42b4727ee72bc4ab675452d52f9408429a60debb39a2c474503386263ad58e545'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Skimly::Application.config.session_store :cookie_store, key: '_skimly_session'
